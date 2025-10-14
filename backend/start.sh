19d85f05ce6b804a204381fd61ebb8c6b149a82d
xiaobai <546142369@qq.com>
2025-10-14 11:47:28 +0800

    update:更新docker compose

--- backend/start.sh
+++ backend/start.sh
@@ -7,15 +7,15 @@ fi
 
 if [ $1 = "app" ]; then
     echo "start app"
-    /usr/local/bin/python -m gunicorn main:app --workers 2 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:$port
+    gunicorn main:app --workers 2 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:$port
 fi
 
 if [ $1 = "celery-worker" ]; then
     echo "start celery worker"
-     /usr/local/bin/python -m celery -A celery_worker.worker.celery worker --pool=gevent -c 10 -l INFO
+    celery -A celery_worker.worker.celery worker --pool=gevent -c 10 -l INFO
 fi
 
 if [ $1 = "celery-beat" ]; then
     echo "start celery beat"
-    /usr/local/bin/python  -m celery -A celery_worker.worker.celery beat -S celery_worker.scheduler.schedulers:DatabaseScheduler -l INFO
+    celery -A celery_worker.worker.celery beat -S celery_worker.scheduler.schedulers:DatabaseScheduler -l INFO
 fi
